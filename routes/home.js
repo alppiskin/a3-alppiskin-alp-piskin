c8846e28f52d26ab5168821b7d004ab2fad5d310
Glitch (a3-alppiskin-alp-piskin) <none>
2019-09-16 05:07:01 +0000

    😂📡 Updated with Glitch

---
+++ routes/home.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const { shallNotPass, shallPass } = require('../config/auth');
+const User = require('../models/user');
+
+// Index page with Login and Register buttons
+router.get('/', shallPass, (req, res) => res.render('index'));
+
+// Login page when user inputs registered credentials
+router.get('/home', shallNotPass, (req, res) => res.render('home', { user: req.user }));
+
+router.post('/home/list', (req, res, next) => {
+  console.log(req.body);
+  let user = User.findById(req.user._id);
+  user.update({_id: req.user._id}, {$push: {watchlist: req.body}}, {upsert: true},function(err, main) {
+        res.redirect('/home');
+  });
+  
+});
+
+router.post('/home/delete/(:id)', (req, res, next) => {
+  console.log(req.params);
+  console.log(req.body);
+  let id = req.params.id;
+  let user = User.findById(req.user._id);
+  user.update({_id: req.user._id}, {$pull: {watchlist: {_id: id}}}, {upsert: true},function(err, main) {
+        res.redirect('/home');
+  });
+});
+
+router.post('/home/edit/(:id)', (req, res, next) => {
+  console.log(req.params);
+  console.log(req.body);
+  let id = req.params.id;
+  let user = User.findById(req.user._id);
+  user.update({_id: req.user._id}, {$pull: {watchlist: {_id: id}}}, {upsert: true},function(err, main) {
+        res.redirect('/home');
+  });
+  user.update({_id: req.user._id}, {$push: {watchlist: req.body}}, {upsert: true},function(err, main) {
+        res.redirect('/home');
+  });
+});
+
+module.exports = router;
